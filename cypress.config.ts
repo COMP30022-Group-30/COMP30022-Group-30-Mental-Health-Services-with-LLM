c4a67c7de4f4960301fd129b684ca83e36eef396
Manyaaag <164144423+Manyaaag@users.noreply.github.com>
2025-10-26 19:47:38 +1100

    Enable screenshot on run failure in Cypress config

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
   e2e: {
     baseUrl: 'https://comp-30022-group-30-mental-health-s.vercel.app',
     video: false,
+    screenshotOnRunFailure: true,
     retries: 1
   }
 })
